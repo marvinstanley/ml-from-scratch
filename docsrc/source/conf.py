2549c5aed532a520f2863a2234e195227ab760f6
Stanley Marvin <stanleymarvin1999@yahoo.com>
2021-07-26 03:24:02 +0700

    change version number

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Stanley Marvin'
 author = 'Stanley Marvin'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
