037d2a6fbefaaab8a21b71ea77f63ea19e9350a7
Stanley Marvin <stanleymarvin1999@yahoo.com>
2021-07-26 03:23:34 +0700

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name = 'ml_scratch',
+      version = '0.0.1',
+      description = "Basic Machine Learning module for personal learning purpose",
+      url = "https://github.com/CollinRooney12/htsprophet",
+      author = "Collin Rooney",
+      author_email = 'stanleymarvin1999@yahoo.com',
+      keywords='ml machine learning machinelearning basic',
+      packages = ['ml_scratch'],
+      zip_safe = False,
+      install_requires = [
+              'numpy'],
+       )
