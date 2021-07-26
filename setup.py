85ca80608f55d0e1369a754a439c57c4db391dc9
Stanley Marvin <stanleymarvin1999@yahoo.com>
2021-07-26 12:28:14 +0700

    fix submodule not installed

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name = 'ml_scratch',
       version = '0.0.1',
       description = "Basic Machine Learning module for personal learning purpose",
-      url = "https://github.com/CollinRooney12/htsprophet",
-      author = "Collin Rooney",
+      url = "https://github.com/marvinstanley/ml-from-scratch",
+      author = "Stanley Marvin",
       author_email = 'stanleymarvin1999@yahoo.com',
       keywords='ml machine learning machinelearning basic',
-      packages = ['ml_scratch'],
+      packages = find_packages(),
+      package_data={'ml_scratch':['*']},
       zip_safe = False,
       install_requires = [
               'numpy'],
